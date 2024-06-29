dce10cebc4f93d08ca9a8659a2d9ab7472db9ee4
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-06-29 14:13:05 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+shopt -s dotglob
 IFS=$'\n\t'
 puid=$(id -u "$USER");
 pgid=$(id -g "$USER");
@@ -7,8 +8,12 @@ if [[ "$EUID" = 0 ]]; then
     echo "do not run from sudo, exiting..."
 	exit 255
 fi
-echo "setting up git..."
-sudo apt install -y git
+if command -v git &> /dev/null; then
+	echo "skipping git..."
+else
+	echo "setting up git..."
+	sudo apt install -y git
+fi
 if [command -v docker &> /dev/null] && [docker compose version &> /dev/null]; then
 	echo "skipping docker..."
 else
@@ -17,16 +22,16 @@ else
 fi
 read -p "input config to set up: " config
 config=${config}
-echo "setting up .env file..."
-sed -i -e "s|<puid>|$puid|g" "$PWD/$config/.env" \
- -e "s|<pgid>|$pgid|g" "$PWD/$config/.env" \
- -e "s|<media>|/$config/media|g" "$PWD/$config/.env" \
- -e "s|<installed>|/$config/install|g" "$PWD/$config/.env"
-echo "installing..."
+echo "setting up installation..."
 cp -R "$config/" "/$config"
 install="/$config/install"
 sudo chown -R $USER:$USER "/$config"
 sudo chown -R "$puid":"$pgid" "/$config"
+echo "setting up .env file..."
+sed -i -e "s|<puid>|$puid|g" "$install/.env" \
+ -e "s|<pgid>|$pgid|g" "$install/.env" \
+ -e "s|<media>|/$config/media|g" "$install/.env" \
+ -e "s|<installed>|$install|g" "$install/.env"
 echo "setting up $config command..."
 sudo sed -i -e "s|<composer>|$install/docker-compose.yaml|g" command \
  -e "s|<name>|$config|g" command
