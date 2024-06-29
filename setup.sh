05a8dc2dea6e833b209e699ec01731cbacd4a728
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-06-29 12:25:42 +1000

    different method

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+puid=$(id -u "$USER");
+pgid=$(id -g "$USER");
+
 if [[ "$EUID" = 0 ]]; then
     echo "do not run from sudo, exiting..."
 	exit 255
@@ -17,52 +20,31 @@ else
 	bash ./setup-docker.sh
 fi
 
-echo "setting up..."
-puid=$(id -u "$USER");
-pgid=$(id -g "$USER");
 read -p "input config to set up: " config
 config=${config}
-group-install="/$config/install"
-group-config="$group-config"
-group-media="/$config/media"
-group-env="./$config/.env"
-group-composer="./$config/docker-compose.yaml"
 
-echo "setting up \"$group-install\"..."
-sudo mkdir -p "$group-install"	
-sudo chown -R $USER:$USER "$group-install"
-sudo chown -R "$puid":"$pgid" "$group-install"
+sudo cp -rf "$config" "/"
+sudo chown -R $USER:$USER "/$config"
+sudo chown -R "$puid":"$pgid" "/$config/"
 
-echo "setting up \"$group-media\"..."
-sudo mkdir -p "$group-media"
-sudo chown -R $USER:$USER "$group-media"
-sudo chown -R "$puid":"$pgid" "$group-media"
-
-echo "setting up \"$group-config\"..."
-sudo mkdir -p "$group-config"
-sudo chown -R $USER:$USER "$group-config"
-sudo chown -R "$puid":"$pgid" "$group-config"
-
-echo "setting up \"$group-composer\" as \"$group-install/docker-compose.yaml\"..."
-cp "$group-composer" "$group-install/docker-compose.yaml"
-echo "setting up \"$group-env\" as \"$group-install/.env\"..."
-cp "$group-env" "$group-install/.env"
+echo "setting up..."
+install="/$config/"
 
 echo "setting up .env file..."
-sudo sed -i -e "s|<puid>|$puid|g" "$group-install/.env" \
- -e "s|<pgid>|$pgid|g" "$group-install/.env" \
- -e "s|<media>|$group-media|g" "$group-install/.env" \
- -e "s|<installed>|$group-install|g" "$group-install/.env"
+sudo sed -i -e "s|<puid>|$puid|g" "$install/.env" \
+ -e "s|<pgid>|$pgid|g" "$install/.env" \
+ -e "s|<media>|/$config/media|g" "$install/.env" \
+ -e "s|<installed>|$install|g" "$install/.env"
  
 echo "setting up $config command..."
-sudo sed -i -e "s|<dockerfile>|$group-install/docker-compose.yaml|g" command \
- -e "s|<installed>|$group-install|g"  \
+sudo sed -i -e "s|<dockerfile>|$install/docker-compose.yaml|g" command \
+ -e "s|<installed>|$install|g"  \
  -e "s|<name>|$config|g"
 sudo cp command "/usr/local/bin/$config"
 sudo chmod +x "/usr/local/bin/$config"
 
 echo "setting up docker compose..."
-docker compose -f "$group-install/docker-compose.yaml" up -d
+docker compose -f "$install/docker-compose.yaml" up -d
 
 echo "done!"
 exit 0
