7e5939426a91610c00224802e2000275e606ecc6
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-07-02 07:22:46 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 installname="homeserv"
-sudo mkdir -p "/$installname"
-sudo chown -R $USER:$USER "/$installname"
+sudo mkdir -p "~/$installname"
+sudo chown -R $USER:$USER "~/$installname"
 if command -v docker &> /dev/null; then
     if ![docker compose version &> /dev/null]; then
         bash ./docker.sh
@@ -15,21 +15,21 @@ if [[ "$EUID" = 0 ]]; then
     exit 0
 fi
 
-if [ ! -d "/$installname" ]; then
-    if mkdir -p "/$installname"; then
+if [ ! -d "~/$installname" ]; then
+    if mkdir -p "~/$installname"; then
         echo
     else
         exit 1
     fi
 fi
-if [ ! -w "/$installname" ] || [ ! -r "/$installname" ]; then
+if [ ! -w "~/$installname" ] || [ ! -r "~/$installname" ]; then
     exit 2
 fi
-installcomposer="/$installname/docker-compose.yaml"
-installenv="/$installname/.env"
+installcomposer="~/$installname/docker-compose.yaml"
+installenv="~/$installname/.env"
 puid=$(id -u "$USER");
 pgid=$(id -g "$USER");
-installmedia="/$installname/media"
+installmedia="~/$installname/media"
 if [ ! -d "$installmedia" ]; then
     if mkdir -p "$installmedia"; then
         echo
@@ -47,27 +47,27 @@ if cp "$installname.env" "$installenv"; then
 else
     exit 5
 fi
-sudo sed -i -e "s|<puid>|$puid|g" "$installenv" \
+sed -i -e "s|<puid>|$puid|g" "$installenv" \
  -e "s|<pgid>|$pgid|g" "$installenv" \
  -e "s|<media>|$installmedia|g" "$installenv" \
- -e "s|<config>|/$installname/config|g" "$installenv"
-sudo sed -i -e "s|<name>|$installname|g" command
+ -e "s|<config>|~/$installname/config|g" "$installenv"
+sed -i -e "s|<name>|$installname|g" command
 docker compose -f "$installcomposer" up -d
-if  ![sudo cp command "/usr/local/bin/$installname" && sudo chmod +x "/usr/local/bin/$installname"]; then
+if  ![sudo cp command "/usr/local/bin~/$installname" && sudo chmod +x "/usr/local/bin~/$installname"]; then
     exit 6
 fi
 if ![sudo chown -R "$puid":"$pgid" "$installmedia"]; then
     exit 7
 fi
-if ![sudo chown -R "$puid":"$pgid" "/$installname"]; then
+if ![sudo chown -R "$puid":"$pgid" "~/$installname"]; then
     exit 8
 fi
-if ![[ -d "/$installname/config" ]]; then
-    if ![sudo mkdir -p "/$installname/config"]; then
+if ![[ -d "~/$installname/config" ]]; then
+    if ![sudo mkdir -p "~/$installname/config"]; then
         exit 9
     fi
 fi
-if ![sudo chown -R "$puid":"$pgid" "/$installname/config"]; then
+if ![sudo chown -R "$puid":"$pgid" "~/$installname/config"]; then
     exit 10
 fi
 exit 11
