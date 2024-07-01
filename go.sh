846ca40a8a2f26feed6a7fff927b5e3c36cf9487
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-07-02 03:06:42 +1000

    Update go.sh

--- go.sh
+++ go.sh
@@ -20,6 +20,7 @@ function getdocker {
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$version $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt update
 	sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+	sudo groupadd docker
 	sudo usermod -aG docker $USER
 	sudo su $USER --group="docker" --session-command="bash go.sh"
 	exit 2
