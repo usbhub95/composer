16926fa57524cd12958e44ccfc0a063ef7b4e7a4
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-07-02 18:22:54 +1000

    add exit codes to docker

--- docker.sh
+++ docker.sh
@@ -2,12 +2,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 version=$(cat /etc/issue.net | awk '{print tolower($1)}')
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/$version/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$version $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -aG docker $USER
-sudo su $USER --session-command "bash install.sh"
+sudo apt update || exit 1
+sudo apt install -y ca-certificates curl gnupg lsb-release || exit 2
+sudo mkdir -p /etc/apt/keyrings || exit 3
+(curl -fsSL https://download.docker.com/linux/$version/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg) || exit 4
+(echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$version $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null) || exit 5
+sudo apt update || exit 6
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin || exit 7
+sudo usermod -aG docker $USER || exit 8
+sudo su $USER --session-command "bash install.sh" || exit 9
+exit 0
--- install.sh
+++ install.sh
@@ -4,26 +4,26 @@ IFS=$'\n\t'
 installname="homeserv"
 sudo mkdir -p "~/$installname" || exit 1
 sudo chown -R $USER:$USER "~/$installname" || exit 2
-(docker &> /dev/null && docker compose version &> /dev/null) || bash ./docker.sh
+(docker &> /dev/null && docker compose version &> /dev/null) || (bash ./docker.sh || echo $?)
 if [[ "$EUID" = 0 ]]; then
-    exit 3
+    exit 4
 fi
-([ ! -d "~/$installname" ] && mkdir -p "~/$installname") || exit 4
-([ ! -w "~/$installname" ] || [ ! -r "~/$installname" ]) || exit 5
+([ ! -d "~/$installname" ] && mkdir -p "~/$installname") || exit 5
+([ ! -w "~/$installname" ] || [ ! -r "~/$installname" ]) || exit 6
 installcomposer="~/$installname/docker-compose.yaml"
 installenv="~/$installname/.env"
 puid=$(id -u "$USER");
 pgid=$(id -g "$USER");
 installmedia="~/$installname/media"
-([ ! -d "$installmedia" ] && mkdir -p "$installmedia") || exit 6
-cp "$installname.docker-compose.yaml" "$installcomposer" || exit 7
-cp "$installname.env" "$installenv" || exit 8
-sed -i -e "s|<puid>|$puid|g" "$installenv" -e "s|<pgid>|$pgid|g" "$installenv" -e "s|<media>|$installmedia|g" "$installenv" -e "s|<config>|~/$installname/config|g" "$installenv" || exit 9
-sed -i -e "s|<name>|$installname|g" command || exit 10
-docker compose -f "$installcomposer" up -d || exit 11
-(sudo cp command "/usr/local/bin~/$installname" && sudo chmod +x "/usr/local/bin~/$installname") || exit 12
-sudo chown -R "$puid":"$pgid" "$installmedia" || exit 13
-sudo chown -R "$puid":"$pgid" "~/$installname" || exit 14
-([ ! -d "~/$installname/config" ] && sudo mkdir -p "~/$installname/config") || exit 15
-sudo chown -R "$puid":"$pgid" "~/$installname/config" || exit 16
+([ ! -d "$installmedia" ] && mkdir -p "$installmedia") || exit 7
+cp "$installname.docker-compose.yaml" "$installcomposer" || exit 8
+cp "$installname.env" "$installenv" || exit 9
+sed -i -e "s|<puid>|$puid|g" "$installenv" -e "s|<pgid>|$pgid|g" "$installenv" -e "s|<media>|$installmedia|g" "$installenv" -e "s|<config>|~/$installname/config|g" "$installenv" || exit 10
+sed -i -e "s|<name>|$installname|g" command || exit 11
+docker compose -f "$installcomposer" up -d || exit 12
+(sudo cp command "/usr/local/bin~/$installname" && sudo chmod +x "/usr/local/bin~/$installname") || exit 13
+sudo chown -R "$puid":"$pgid" "$installmedia" || exit 14
+sudo chown -R "$puid":"$pgid" "~/$installname" || exit 15
+([ ! -d "~/$installname/config" ] && sudo mkdir -p "~/$installname/config") || exit 16
+sudo chown -R "$puid":"$pgid" "~/$installname/config" || exit 17
 exit 0
