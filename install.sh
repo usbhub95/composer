d81759894f8d3a7bb232bd21e6509e1e4e610986
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-07-05 17:07:46 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ "$EUID" = 0 ]]; then
     exit 1
 fi
 config="homeserv"
-installdir="~/$config"
+installdir="/home/$USER/$config"
 composer="$installdir/docker-compose.yaml"
 env="$installdir/.env"
 puid=$(id -u "$USER");
